606d3742bf6f9394a58973cc51f7c6e4563283fe
Caden Maxwell <cadenr.maxwell@gmail.com>
2023-12-02 15:40:52 -0700

    Much better!

--- 01/part2.c
+++ 01/part2.c
@@ -4,8 +4,7 @@
 #include <string.h>
 #include <ctype.h>
 
-void convertFirstCharToDigit(char* match, int matchLen);
-void convertLastCharToDigit(char* match, int matchLen);
+void convertCharToDigit(char* match, int matchLen, int isLast);
 
 int main()
 {
@@ -25,15 +24,16 @@ int main()
 
     char line[128];
     int sum = 0;
-    while (fgets(line, sizeof(line), fp)) {
+    while (fgets(line, sizeof(line), fp))
+    {
         regexec(&regex, line, 1, &match, 0);
         int matchLen = match.rm_eo - match.rm_so;
 
         // If first or last char is alpha, convert it to a digit
         if (isalpha(line[match.rm_so]))
-            convertFirstCharToDigit(&line[match.rm_so], matchLen);
+            convertCharToDigit(&line[match.rm_so], matchLen, 0);
         if (isalpha(line[match.rm_eo - 1]))
-            convertLastCharToDigit(&line[match.rm_so], matchLen);
+            convertCharToDigit(&line[match.rm_so], matchLen, 1);
 
         digits[0] = line[match.rm_so];
         digits[1] = line[match.rm_eo - 1];
@@ -50,63 +50,18 @@ int main()
     return 0;
 }
 
-/**
- * Convert the first character of a match to a digit based on the word it spells
-*/
-void convertFirstCharToDigit(char* match, int matchLen)
+void convertCharToDigit(char* match, int matchLen, int isLast)
 {
-    char first2[3];
-    first2[0] = match[0];
-    first2[1] = match[1];
-    first2[2] = '\0';
-
-    if (strcmp(first2, "on") == 0)
-        match[0] = '1';
-    else if (strcmp(first2, "tw") == 0)
-        match[0] = '2';
-    else if (strcmp(first2, "th") == 0)
-        match[0] = '3';
-    else if (strcmp(first2, "fo") == 0)
-        match[0] = '4';
-    else if (strcmp(first2, "fi") == 0)
-        match[0] = '5';
-    else if (strcmp(first2, "si") == 0)
-        match[0] = '6';
-    else if (strcmp(first2, "se") == 0)
-        match[0] = '7';
-    else if (strcmp(first2, "ei") == 0)
-        match[0] = '8';
-    else if (strcmp(first2, "ni") == 0)
-        match[0] = '9';
-}
-
-/**
- * Convert the last character of a match to a digit based on the word it spells
-*/
-void convertLastCharToDigit(char* match, int matchLen)
-{
-    char last3[4];
-    last3[0] = match[matchLen - 3];
-    last3[1] = match[matchLen - 2];
-    last3[2] = match[matchLen - 1];
-    last3[3] = '\0';
-
-    if (strcmp(last3, "one") == 0)
-        match[matchLen - 1] = '1';
-    else if (strcmp(last3, "two") == 0)
-        match[matchLen - 1] = '2';
-    else if (strcmp(last3, "ree") == 0)
-        match[matchLen - 1] = '3';
-    else if (strcmp(last3, "our") == 0)
-        match[matchLen - 1] = '4';
-    else if (strcmp(last3, "ive") == 0)
-        match[matchLen - 1] = '5';
-    else if (strcmp(last3, "six") == 0)
-        match[matchLen - 1] = '6';
-    else if (strcmp(last3, "ven") == 0)
-        match[matchLen - 1] = '7';
-    else if (strcmp(last3, "ght") == 0)
-        match[matchLen - 1] = '8';
-    else if (strcmp(last3, "ine") == 0)
-        match[matchLen - 1] = '9';
+    char* words[] = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    int lengths[] = { 3, 3, 5, 4, 4, 3, 5, 5, 4 };
+    for (int i = 0; i < 9; i++)
+    {
+        int offset = isLast ? matchLen - lengths[i] : 0;
+        if (strncmp(match + offset, words[i], lengths[i]) == 0)
+        {
+            if (isLast) match[matchLen - 1] = i + '1';
+            else match[0] = i + '1';
+            break;
+        }
+    }
 }
